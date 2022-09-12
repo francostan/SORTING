ed29fe9171ade0675db17c824609433f650f9a65
francostan <francostan98@hotmail.com>
2022-09-12 10:57:22 -0300

    cambios bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -3,17 +3,26 @@ function swap(array, i) {
   let num2 = array[i + 1];
   array[i] = num2;
   array[i + 1] = num1;
+  //forma del review
+  // [array[i], array[i + 1]] = [array[i + 1], array[i]];
+
 
   return array;
 }
 
 function bubbleSort(array) {
-  for (j = 0; j < array.length; j++) {
+//usamos dos for para pararnos en un elemento del array
+// e ir comparandolo con todos los elementos de este
+// si tuvieramos [5,4,3,2,1], en la primer vuelta del for de arriba nos quedaria asi [4,3,2,1,5]
+  for (let j = 0; j < array.length; j++) {
     for (i = 0; i < array.length; i++) {
-      if (array[i] > array[i + 1]) {
+      //manejamos la posibilidad de que el array sea vacio/undefined
+      if (array[i] > array[i + 1] && array[i + 1]) {
         swap(array, i);
       }
     }
   }
   return array;
+  //hay otra forma en la que podemos hacer el for de arriba dando vuelta j = array.length - 1, etc
+  //la podemos ver en review min 40:00
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,9 +1,13 @@
 //Funcion bubbleSort: deberia volver un array ordenado.
-
-/*describe("Bubble Sort", function () {
+//la parte de describe es la denominada suite, que recibe un nombre y callback (arrow function) o function normal
+describe("Bubble Sort", function () {
+  //la parte de it es la denominada spec, que recibe un nombre(string)y callback (arrow function) o function normal
     it("Maneja arreglos vacios", function () {
       expect(bubbleSort([])).toEqual([]);
     });
+    //aqui validamos lo que necesitemos, esperamos que algo sea igual a algo
+    //puedo ir ejecturando otras funciones
+
     it("Ordena un array", function () {
       expect(bubbleSort([2, 4, 1, 3])).toEqual([1, 2, 3, 4]);
     });
@@ -13,6 +17,10 @@
     it("Un arreglo a la inversa, deberia de estar ordenado", function () {
       expect(bubbleSort([4, 2, 1, 3])).toEqual([1, 2, 3, 4]);
     });
+    //Funcion Spy on: para saber cuantas veces se ejecuto una funcion
+    //tambien cuenta los parametros que se le pasan a la funcion y mas
+    //SpyOn(window(si esta en el contexto global), "nombre de la funcion").and.callThrough();
+    //expect(window.nombre de la funcion.calls.count()).toEqual(1);
   });
   
   //Funcion Swap
@@ -22,4 +30,4 @@
       expect(swap([90, 20], 0)).toEqual([20, 90]);
     });
   });
-  */
+  
